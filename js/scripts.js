c740d1580f84259824f11d8244a80c8d5d8a95f4
Winter Runion <winterrunion@gmail.com>
2020-02-02 17:50:51 -0800

    Add vanilla JS code to clear input area after value has been read.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,13 @@ $(document).ready(function() {
   //console.log('jquery is working');
   $("form#number-form").submit(function(event) {
     //console.log('form has submitted');
+    let inputValue = $("#number-input").val();
+    console.log(inputValue);
 
-    
+    //Clear the input field
+    let inputArea = document.getElementById("number-input");
+    inputArea.value = "";
+    //Prevent the form from attempting to send data to a server
     event.preventDefault();
   });
 
