64d358ad922807974c7ca93bde20e4dfc2b3941d
Winter Runion <winterrunion@gmail.com>
2020-02-02 17:43:31 -0800

    Add jQuery logic to get form values on submit. Build a boostrap form with number input in index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@
 
 //UI Logic
 $(document).ready(function() {
-  $("#")
+  //console.log('jquery is working');
+  $("form#number-form").submit(function(event) {
+    //console.log('form has submitted');
+
+    
+    event.preventDefault();
+  });
 
 });
