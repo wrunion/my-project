99fcc3928bb46845d69725ece7e71aa3ab8bfb43
Winter Runion <winterrunion@gmail.com>
2020-02-02 18:46:22 -0800

    Add results div and append result based on input.

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,23 @@ $(document).ready(function() {
   //console.log('jquery is working');
   $("form#number-form").submit(function(event) {
     //console.log('form has submitted');
+
     let inputValue = $("#number-input").val();
     //console.log(inputValue);
     if (inputValue % 15 ===0) {
       console.log("ping-pong");
+      $("#result").append("Ping Pong!<br>");
     } else if (inputValue % 3 === 0) {
       console.log("ping");
+      $("#result").append("Ping!<br>");
     } else if (inputValue % 5 === 0) {
       console.log("pong");
+      $("#result").append("Pong!<br>");
     } else {
-      console.log("Please try again.");
+      console.log("Please try again");
+      $("#result").append("Guess again!<br>");
     }
-
+    $("#results-div").show();
 
 
 
@@ -25,6 +30,10 @@ $(document).ready(function() {
     inputArea.value = "";
     //Prevent the form from attempting to send data to a server
     event.preventDefault();
+  });
+
+  $("#button-reload").click(function() {
+    location.reload();
   });
 
 });
