b97ea667fc9f1db9fceddce64f111a65ff1d7b7b
Winter Runion <winterrunion@gmail.com>
2020-02-02 17:56:17 -0800

    Complete core functionality of the assignment. Numbers divisible by 3 output ping (to the console), numbers divisible by 5 output pong, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
   $("form#number-form").submit(function(event) {
     //console.log('form has submitted');
     let inputValue = $("#number-input").val();
-    console.log(inputValue);
+    //console.log(inputValue);
+    if (inputValue % 15 ===0) {
+      console.log("ping-pong");
+    } else if (inputValue % 3 === 0) {
+      console.log("ping");
+    } else if (inputValue % 5 === 0) {
+      console.log("pong");
+    } else {
+      console.log("Please try again.");
+    }
+
+
+
 
     //Clear the input field
     let inputArea = document.getElementById("number-input");
