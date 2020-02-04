9f0f1c12262cd13ddcdcff26b30d5eb70db4ee83
wrunion <winterrunion@gmail.com>
2020-02-04 15:43:14 -0800

    Add function to push numbers to a resultArray for display

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
 //Business Logic
 
-function returnStrings(number, string1, string2, string3) {
-  return(number + string1 + string2 + string3);
+let printArray = [];
+
+function kubricWasAnAbusiveMisogynist() {
+  printArray.push("0");
+  printArray.push("Beep!");
+  printArray.push("Boop!");
+  printArray.push(`I refuse to quote Kubric for any reason, so enjoy these inspirational quotes from a modern day poet laureate instead:`);
 }
 
+// function returnStrings(number, a, b, c) {
+//   let firstOne = "First One";
+//   let firstTwo = "First Two";
+//   let firstThree = "Fuck Kubric";
+//   let a = "a"
+//   let b = "b"
+//   let c = "c";
+//   let returnArray = [];
+  
+//   for (let i = 0; i <= number; i++) {
+//     console.log(i);
+//     returnArray.push(i);
+//   }
+//   console.log(returnArray);
+//   return returnArray;
+// }
+
 //Change the below values, based on which skin/theme is being used
-let a = "TiK";
-let b = "ToK";
-let c = "Now the party don't start 'til I walk in.";
-let d = `I refuse to quote Kubric for any reason, so here's a collection of quotes from a modern day poet laureate instead:`;
+// let a = "TiK";
+// let b = "ToK";
+// let c = "Now the party don't start 'til I walk in.";
+// let d = `I refuse to quote Kubric for any reason, so here's a collection of quotes from a modern day poet laureate instead:`;
 
 //UI Logic
 $(document).ready(function() {
@@ -21,41 +43,33 @@ $(document).ready(function() {
     console.log(typeof inputValue);
    // $("#result").append(returnStrings(inputValue, a, b, c));
 
-    for (let i = 0; i<=inputValue; i++) {
-      if (i === 3) {
-        console.log(d);
-        $("#result").append(`<ul>${d}</ul>`);
-      } else if (i.toString().includes("3")) {
-        console.log(c);
-        $("#result").append(`<ul>${c}</ul>`);
-      } else if (i.toString().includes("2")) {
-       console.log(b); 
-       $("#result").append(`<ul>${b}</ul>`);
-      } else if (i.toString().includes("1")) {
-        console.log(a);
-        $("#result").append(`<ul>${a}</ul>`);
-      } else {
-        console.log(i.toString());
-        $("#result").append(`<ul>${i.toString()}</ul>`);
-      }
-    }
-
-    // for (let i = 1; i <= inputValue; i++) {
-    //   if (i % 15 ===0) {
-    //     console.log("ping-pong");
-    //     $("#result").append("Ping Pong!<br>");
-    //   } else if (i % 3 === 0) {
-    //     console.log("ping");
-    //     $("#result").append("Ping!<br>");
-    //   } else if (i % 5 === 0) {
-    //     console.log("pong");
-    //     $("#result").append("Pong!<br>");
+    // for (let i = 0; i<=inputValue; i++) {
+    //   if (i === 3) {
+    //     console.log(d);
+    //     $("#result").append(`<ul>${d}</ul>`);
+    //   } else if (i.toString().includes("3")) {
+    //     console.log(c);
+    //     $("#result").append(`<ul>${c}</ul>`);
+    //   } else if (i.toString().includes("2")) {
+    //    console.log(b); 
+    //    $("#result").append(`<ul>${b}</ul>`);
+    //   } else if (i.toString().includes("1")) {
+    //     console.log(a);
+    //     $("#result").append(`<ul>${a}</ul>`);
     //   } else {
-    //     console.log(i);
-    //     $("#result").append(`<span class="text-secondary">${i}</span><br>`);
+    //     console.log(i.toString());
+    //     $("#result").append(`<ul>${i.toString()}</ul>`);
     //   }
     // }
 
+    //returnStrings(10);
+
+    kubricWasAnAbusiveMisogynist();
+    console.log(printArray);
+
+    printArray.forEach(function(e) {
+      $("#result-ul").append(`<li>${e}</li>`)
+    });
 
     $("#results-div").show();
 
